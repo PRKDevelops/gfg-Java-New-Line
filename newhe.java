f06b09a91ec0b8e28f8670a4d7a33d40117173da
Pranav Khare <124041839+PRKDevelops@users.noreply.github.com>
2024-07-03 21:40:42 +0530

    Add files via upload

---
+++ newhe.java
@@ -0,0 +1,15 @@
+class Geeks{
+	//User function Template for Java
+	    
+	    // Function to print each word in single line
+	    static void printIndividualLine(){
+	        
+	        // Your code here
+	        System.out.println("Geeks");
+	        System.out.println("for");
+	        System.out.println("Geeks");
+	        
+	    } }
+
+    
+	
